38591360dc8ba3b9cdc041a42d5117e09623a9a3
rtbrisen <rtbrisen@ucsc.edu>
2023-12-04 16:32:12 -0800

    Adding DenseLayer class and applying to nnfs test data

--- DenseLayerClass.py
+++ DenseLayerClass.py
@@ -3,7 +3,7 @@ import numpy as np
 class Dense_Layer:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
-        self.biasese = np.zeros((1, n_neurons))
+        self.biases = np.zeros((1, n_neurons))
         
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
--- Importing_test_data.py
+++ Importing_test_data.py
@@ -3,13 +3,26 @@ import nnfs
 from nnfs.datasets import spiral_data
 import matplotlib.pyplot as plt
 
-"""
-    init method sets random seed to 0, creates a
-    float32 dtype default, & overrides original
-    dot product from numpy
-"""
+from DenseLayerClass import Dense_Layer
+
+
+
+# nnfs.init() method sets random seed to 0, creates a
+# float32 dtype default, & overrides original
+# dot product from numpy
 nnfs.init() 
+
 X, y = spiral_data(samples=100, classes=3)
 
-plt.scatter(X[:,0],X[:,1], c=y, cmap="brg")
-plt.show()
+print(X)
+
+dense1 = Dense_Layer(2, 3)
+dense1.forward(X)
+
+print(dense1.output[:5])
+
+
+
+
+# plt.scatter(X[:,0],X[:,1], c=y, cmap="brg")
+# plt.show()
