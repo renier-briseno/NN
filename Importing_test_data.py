788bb7d42a502d096b483440d311ffb8aef565ad
rtbrisen <rtbrisen@ucsc.edu>
2023-12-06 19:07:30 -0800

    Adding ReLU activation demo to Spiral code

--- Importing_test_data.py
+++ Importing_test_data.py
@@ -4,6 +4,7 @@ from nnfs.datasets import spiral_data
 import matplotlib.pyplot as plt
 
 from DenseLayerClass import Dense_Layer
+from ReLU_Class import ReLU_Activation
 
 
 
@@ -14,13 +15,15 @@ nnfs.init()
 
 X, y = spiral_data(samples=100, classes=3)
 
-print(X)
 
 dense1 = Dense_Layer(2, 3)
+activation1 = ReLU_Activation()
+
 dense1.forward(X)
+activation1.forward(dense1.output)
 
 print(dense1.output[:5])
-
+print(activation1.output[:5])
 
 
 
